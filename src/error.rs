221527c0e272d7fb2ecad122e0aa92c21a43f407
Hugues de Valon <hugues.devalon@arm.com>
2021-01-27 11:17:44 +0000

    Revert "Remove spiffe-based feature from master"
    This reverts commit 5e8702ff0b6b4c4930acf0193f6547e3419b2d38.
    
    Signed-off-by: Hugues de Valon <hugues.devalon@arm.com>

--- src/error.rs
+++ src/error.rs
@@ -42,6 +42,9 @@ pub enum ClientErrorKind {
     NoAuthenticator,
     /// Required parameter was not provided
     MissingParam,
+    /// Error while using the SPIFFE Workload API
+    #[cfg(feature = "spiffe-auth")]
+    Spiffe(spiffe::workload::Error),
 }
 
 impl From<ClientErrorKind> for Error {
@@ -65,6 +68,8 @@ impl fmt::Display for ClientErrorKind {
             ClientErrorKind::NoProvider => write!(f, "client is missing an implicit provider"),
             ClientErrorKind::NoAuthenticator => write!(f, "service is not reporting any authenticators or none of the reported ones are supported by the client"),
             ClientErrorKind::MissingParam => write!(f, "one of the `Option` parameters was required but was not provided"),
+            #[cfg(feature = "spiffe-auth")]
+            ClientErrorKind::Spiffe(error) => error.fmt(f),
         }
     }
 }
