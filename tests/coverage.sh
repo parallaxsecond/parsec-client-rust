8606ca74c4500a7a5ece9aedb1078c776b0c3eeb
Tomás González <tomasagustin.gonzalezorlando@arm.com>
2023-10-06 10:40:32 +0100

    ci/nightly: Run parsec-mock for coverage tests
    This commit fixes nightly issues:
    Certain tests ran by coverage.sh require a mock service to have
    started before the tests are actually ran.
    
     * Clone and start the parsec-mock service for coverage testing.
    
    Signed-off-by: Tomás González <tomasagustin.gonzalezorlando@arm.com>

--- tests/coverage.sh
+++ tests/coverage.sh
@@ -7,6 +7,21 @@ set -euf -o pipefail
 
 cargo install cargo-tarpaulin
 
+######################
+# Start Mock Service #
+######################
+CURRENT_PATH=$(pwd)
+cd parsec-mock-0.1.1
+python parsec_mock/parsec_mock.py --parsec-socket $CURRENT_PATH/parsec_mock.sock &
+while [[ ! -S $CURRENT_PATH/parsec_mock.sock ]]; do
+	sleep 5
+done
+cd ..
+export PARSEC_SERVICE_ENDPOINT="unix://$CURRENT_PATH/parsec_mock.sock"
+
+######################
+# Run tests          #
+######################
 cargo tarpaulin --tests --out Xml --exclude-files="src/core/testing/*"
 
 bash <(curl -s https://codecov.io/bash)
